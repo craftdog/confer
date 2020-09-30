5474aadf38f6c33659f2bc4ce53974a1ff6ed866
aaronalberg <aalberg2@illinois.edu>
2020-09-30 12:22:48 -0500

    routing

--- api/src/Routing.kt
+++ api/src/Routing.kt
@@ -1,12 +1,32 @@
 package com.confer.api
 
 import io.ktor.application.call
+import io.ktor.http.*
 import io.ktor.locations.Location
-import io.ktor.response.respondText
+import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Routing.routingRoot() {
+    participantRouting()
+    modelRouting()
+}
+
+fun Routing.modelRouting() {
+    route("/models/items") {
+        get {
+            call.respondText("ALL ITEMS")
+        }
+
+        get("/{id}") {
+            println("ITEM: " + call.parameters["id"])
+            call.respond(HttpStatusCode.Accepted, "hello")
+        }
+    }
+}
+
+fun Routing.participantRouting() {
     route("/participant") {
+
         get("/info/{id}") {
             val id = call.parameters["id"]
             call.respondText("id get")
@@ -44,108 +64,5 @@ fun Routing.routingRoot() {
                 call.respondText("{\"test\":\"increase\"}")
             }
         }
-
-
-
-
-
     }
-
-    route("/models/items") {
-        get {
-            call.respondText("ALL ITEMS")
-        }
-
-        get("/{id}") {
-            println("ITEM: " + call.parameters["id"])
-            call.respondText("specific item")
-        }
-    }
-
-    /*
-    method(HttpMethod.Get) {
-        route("/participant") {
-
-        }
-    }
-    get("/test") {
-        call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-
-    }
-    get("/participant/info/{id}") {
-        val id = call.parameters["id"]
-        println(id)
-    }
-
-    get("/participant") {
-
-    }
-
-
-
-
-
-    get("/html-dsl") {
-        call.respondHtml {
-            body {
-                h1 { +"HTML" }
-                ul {
-                    for (n in 1..10) {
-                        li { +"$n" }
-                    }
-                }
-            }
-        }
-    }
-
-    // Static feature. Try to access `/static/ktor_logo.svg`
-    static("/static") {
-        resources("static")
-    }
-
-    get<MyLocation> {
-        call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-    }
-    // Register nested routes
-    get<Type.Edit> {
-        call.respondText("Inside $it")
-    }
-    get<Type.List> {
-        call.respondText("Inside $it")
-    }
-
-    get("/session/increment") {
-        val session = call.sessions.get<MySession>() ?: MySession()
-        call.sessions.set(session.copy(count = session.count + 1))
-        call.respondText("Counter is ${session.count}. Refresh to increment.")
-    }
-
-    webSocket("/myws/echo") {
-        send(Frame.Text("Hi from server"))
-        while (true) {
-            val frame = incoming.receive()
-            if (frame is Frame.Text) {
-                send(Frame.Text("Client said: " + frame.readText()))
-            }
-        }
-    }
-
-    get("/json/gson") {
-        call.respond(mapOf("hello" to "world"))
-    }
-
-     */
-}
-
-
-
-@Location("/location/{name}")
-class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-
-@Location("/type/{name}") data class Type(val name: String) {
-    @Location("/edit")
-    data class Edit(val type: Type)
-
-    @Location("/list/{page}")
-    data class List(val type: Type, val page: Int)
 }
